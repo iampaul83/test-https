91be26922e14ed50890615685852dc142846557b
Paul Tsai <paul.tsai@cherri.tech>
2017-10-19 17:20:51 +0800

    fix bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json({
+app.use(bodyParser.text({
     type: '*/*'
 }))
 // app.use(bodyParser.urlencoded({
@@ -11,14 +11,14 @@ app.use(bodyParser.json({
 
 app.post('/post_back_url', (req, res, next) => {    
     console.log(req.originalUrl)
-    console.log(JSON.stringify(req.body))
+    console.log(req.body)
     console.log(req.headers)
     res.json(req.body)
 })
 
 app.post('/result_url', (req, res, next) => {
     console.log(req.originalUrl)    
-    console.log(JSON.stringify(req.body))
+    console.log(req.body)
     console.log(req.headers)
     res.sendStatus(200)
 })
