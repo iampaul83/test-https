913ddba42f11a57f8a645e05ef8647334898a9ad
Paul Tsai <paul.tsai@cherri.tech>
2017-10-19 16:50:45 +0800

    fix log

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ const bodyParser = require('body-parser')
 
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
 
 app.post('/post_back_url', (req, res, next) => {    
-    console.log('/post_back_url')
-    console.log(JSON.stringify(res.body))
-    return res.json(req.body)
+    console.log(req.originalUrl)
+    console.log(JSON.stringify(req.body))
+    res.json(req.body)
 })
 
 app.post('/result_url', (req, res, next) => {
-    console.log('/result_url')
-    console.log(JSON.stringify(res.body))
+    console.log(req.originalUrl)    
+    console.log(JSON.stringify(req.body))
     res.sendStatus(200)
 })
 
